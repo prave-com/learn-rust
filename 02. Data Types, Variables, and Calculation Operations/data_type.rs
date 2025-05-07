dec845f1db778f32d1c2fc51c281421eff63a28e
Kira <117554978+kiraware@users.noreply.github.com>
2025-05-07 13:44:45 +0700

    rust data types

---
+++ 02. Data Types, Variables, and Calculation Operations/data_type.rs
@@ -0,0 +1,39 @@
+fn main() {
+    // Integer Data Types
+    let i8_var: i8 = -128;
+    let u8_var: u8 = 255;
+    let i16_var: i16 = -32_768;
+    let u16_var: u16 = 65_535;
+    let i32_var: i32 = -2_147_483_648;
+    let u32_var: u32 = 4_294_967_295;
+    let i64_var: i64 = -9_223_372_036_854_775_808;
+    let u64_var: u64 = 18_446_744_073_709_551_615;
+    let i128_var: i128 = -170_141_183_460_469_231_731_687_303_715_884_105_728;
+    let u128_var: u128 = 340_282_366_920_938_463_463_374_607_431_765_592_591;
+
+    // Floating Point Data Type
+    let f32_var: f32 = 3.14;
+    let f64_var: f64 = 3.141592653589793;
+
+    // Char Data Type
+    let char_var: char = '👍';
+
+    // Boolean Data Type
+    let bool_var: bool = true;
+
+    // Printing each variable with its type
+    println!("i8_var: {} (i8)", i8_var);
+    println!("u8_var: {} (u8)", u8_var);
+    println!("i16_var: {} (i16)", i16_var);
+    println!("u16_var: {} (u16)", u16_var);
+    println!("i32_var: {} (i32)", i32_var);
+    println!("u32_var: {} (u32)", u32_var);
+    println!("i64_var: {} (i64)", i64_var);
+    println!("u64_var: {} (u64)", u64_var);
+    println!("i128_var: {} (i128)", i128_var);
+    println!("u128_var: {} (u128)", u128_var);
+    println!("f32_var: {} (f32)", f32_var);
+    println!("f64_var: {} (f64)", f64_var);
+    println!("char_var: {} (char)", char_var);
+    println!("bool_var: {} (bool)", bool_var);
+}
