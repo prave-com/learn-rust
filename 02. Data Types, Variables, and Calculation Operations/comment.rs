79f8cb5ab19a39f1defa76139da91a8deb4d92b7
Kira <117554978+kiraware@users.noreply.github.com>
2025-05-07 11:36:45 +0700

    add comment

---
+++ 02. Data Types, Variables, and Calculation Operations/comment.rs
@@ -0,0 +1,15 @@
+fn main() {
+    // This is a single-line comment
+    // println!("{}", 5);
+    // println!("{}", 6);
+
+    /*
+        This is a multi-line comment.
+        It can span across multiple lines.
+        Rust will ignore everything inside this block.
+        println!("{}", 10);
+        println!("{}", 15);
+    */
+
+    /* /* This is nested comment */ */
+}
